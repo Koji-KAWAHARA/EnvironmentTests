252f28bc1bbb98c21c365fe30241a2539affc1de
Koji-KAWAHARA <43766680+Koji-KAWAHARA@users.noreply.github.com>
2019-07-20 17:26:12 +0900

    Tried to use dictionary+ansync io but not seem like working
    Using dictionary type data carrier from the start might be a solution.

--- Test/Cython/MatrixOperetion/cython_matrix.pyx
+++ Test/Cython/MatrixOperetion/cython_matrix.pyx
@@ -28,7 +28,7 @@ def para_AdB():
 
   c1 = np.zeros(N, dtype = DTYPE)
   c2 = c1; c3 = c1; c4 = c1
-  dict = {1:c1, 2:c2, 3:c3, 4:c4}
+  dict = {1:[c1,A,B], 2:[c2,A,B], 3:[c3,A,B], 4:[c4,A,B]}
   begin = time.time()
 
   """Simple execute"""
@@ -40,14 +40,20 @@ def para_AdB():
   """
 
   """asyncio"""
-  async def async_process(A,B):
+  async def async_process(dict,c1,c2,c3):
+    global c1,c2,c3
     for i in range(1,5):
-      dict[i] = onsite_ddot(A,B)
+      dict[i][0] = onsite_ddot(dict[i][1],dict[i][2])
+      print(dict[i])
+    c1 = dict[1][0]
+    c2 = dict[2][0]
+    c3 = dict[3][0]
     return True
+
   bigloop=asyncio.get_event_loop()
-  bigloop.run_until_complete(async_process(A,B))
+  bigloop.run_until_complete(async_process(dict,c1,c2,c3))
 
-
+  print(dict[1])
   end = time.time()
   print(end-begin)
   return c1
