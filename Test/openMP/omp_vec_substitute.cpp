8c90cf4fffa4ce7b212567188ca670d396b42dab
Koji-KAWAHARA <catman.kozy@gmail.com>
2022-08-19 11:22:26 +0900

    Simple openmp test. Works just fine!

--- Test/openMP/omp_vec_substitute.cpp
+++ Test/openMP/omp_vec_substitute.cpp
@@ -1,8 +1,8 @@
 //test program for vector substitution using openmp
 /*
-Matrix M = Matrix_of_size_(N,M)
-Vector V = Vector_of_size_N*M   : flattend version of M
-V(M*i+j) = M(i,j)
+Matrix Mat = Matrix_of_size_(N,M)
+Vector Vec = Vector_of_size_N*M   : flattend version of M
+Vec(M*i+j) = Mat(i,j)
 */
 
 #define EIGEN_USE_MKL_ALL
@@ -15,10 +15,89 @@ V(M*i+j) = M(i,j)
 #include <Eigen/Dense>      //for dense matrix
 #include <Eigen/SparseCore> //for sparse matrix
 
-int N=4;
-int M=4;
+#include <omp.h>
+
 
 int main(void){
-    //sequential version
+    omp_set_num_threads(2);
+    int N=4;
+    int M=4;
+    Eigen::MatrixXd Mat = Eigen::MatrixXd::Constant(N,M,1); //all 1;
+    Mat(0,0)=1; Mat(1,1)=2; Mat(2,2)=3; Mat(3,3)=4; 
+    Eigen::VectorXd Vec = Eigen::VectorXd::Zero(N*M);
+    std::cout<<Mat<<std::endl;
+    /*
+     #######                                                         
+     #       #        ##   ##### ##### ###### #    # # #    #  ####  
+     #       #       #  #    #     #   #      ##   # # ##   # #    # 
+     #####   #      #    #   #     #   #####  # #  # # # #  # #      
+     #       #      ######   #     #   #      #  # # # #  # # #  ### 
+     #       #      #    #   #     #   #      #   ## # #   ## #    # 
+     #       ###### #    #   #     #   ###### #    # # #    #  ####  
+                                                                             
+    */
+    //sequential
+    for(int i=0; i<Mat.rows(); i++){
+        for(int j=0; j<Mat.cols();j++){
+            Vec(Mat.cols()*i+j)=Mat(i,j);
+        }
+    }
+    std::cout << "seq"<<std::endl << Vec.transpose()<< std::endl;
 
+    //parallel
+    #pragma omp parallel for
+    for(int i=0; i<Mat.rows(); i++){
+        for(int j=0; j<Mat.cols();j++){
+            Vec(Mat.cols()*i+j) = Mat(i,j);
+        }
+    }
+    //index i,j loop内部で生成してるので自動的にprivate変数。
+
+    std::cout << "parallel"<<std::endl << Vec.transpose() << std::endl;
+
+    /*                                                                                                     
+    #####   ####   ####   ####  # #####  #      ######     ####   ####  #    # ###### #      #  ####  ##### 
+    #    # #    # #      #      # #    # #      #         #    # #    # ##   # #      #      # #    #   #   
+    #    # #    #  ####   ####  # #####  #      #####     #      #    # # #  # #####  #      # #        #   
+    #####  #    #      #      # # #    # #      #         #      #    # #  # # #      #      # #        #   
+    #      #    # #    # #    # # #    # #      #         #    # #    # #   ## #      #      # #    #   #   
+    #       ####   ####   ####  # #####  ###### ######     ####   ####  #    # #      ###### #  ####    #   
+    */
+
+    //multiple addition works just fine
+    //if and only if it's not recurrent calculation.
+    std::cout << "Add 1 then Add 2 to each element"<<std::endl;
+    std::cout << "similar to grad making situation"<<std::endl;
+    
+    #pragma omp parallel for
+    for(int i=0; i<Mat.rows(); i++){
+        for(int j=0; j<Mat.cols();j++){
+            Vec(Mat.cols()*i+j) += 1;
+            Vec(Mat.cols()*i+j) += 2;
+        }
+    }
+    std::cout << "parallel"<<std::endl << Vec.transpose() << std::endl;
+
+    //adding locally defined value
+    std::cout << "Add Mat(i,j) to each element"<<std::endl;   
+    #pragma omp parallel for
+    for(int i=0; i<Mat.rows(); i++){
+        for(int j=0; j<Mat.cols();j++){
+            double add = Mat(i,j);
+            Vec(Mat.cols()*i+j) += add;
+        }
+    }
+    std::cout << "parallel"<<std::endl << Vec.transpose() << std::endl;
+
+    std::cout << "Add itself( Vec(self) ) to each element"<<std::endl;   
+    #pragma omp parallel for
+    for(int i=0; i<Mat.rows(); i++){
+        for(int j=0; j<Mat.cols();j++){
+            double add = Vec(Mat.cols()*i+j);
+            Vec(Mat.cols()*i+j) += add;
+        }
+    }
+    std::cout << "parallel"<<std::endl << Vec.transpose() << std::endl;
+
+    return 0;
 }
